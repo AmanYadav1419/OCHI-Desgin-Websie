a0c74f53cadf2292fea5974c432be13ce6eed4f6
unknown <amanyadav16544@gmail.com>
2024-02-09 22:12:40 +0530

    Marquee text animation only remains & start of About Page half is Done

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
+        <h1 className="font-['Neue_Montreal'] text-[4vw] leading-[4.5vw] tracking-tight">
+            Ochi is a strategic partner for fast-growing tech bussines that need
+            to raise funds, sell products, explain complex ideas, and hire great
+            people.
+        </h1>
+        {/* middle part is remening this is ony for middle part completion */}
+
+        <div className='w-full border-t-[1px] border-[#a1b562]'>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,8 +4,8 @@ const Marquee = () => {
   return (
     <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
         <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
-            <h1 className='text-[16vw] leading-none font-["Founders_Grotesk"] uppercase -mb-20 pt-10 font-semibold'>We are Ochi</h1>
-            <h1 className='text-[16vw] leading-none font-["Founders_Grotesk"] uppercase -mb-20 pt-10 font-semibold'>We are Ochi</h1>
+            <h1 className='text-[24vw] leading-none font-["Founders_Grotesk"] uppercase -mb-[7vw] pt-10 font-semibold'>We are Ochi</h1>
+            <h1 className='text-[24vw] leading-none font-["Founders_Grotesk"] uppercase -mb-[7vw] pt-10 font-semibold'>We are Ochi</h1>
         </div>
     </div>
   )
