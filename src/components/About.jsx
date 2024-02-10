83bb78d880f81e3fed0ef7c7594e5b6d29300bff
unknown <amanyadav16544@gmail.com>
2024-02-10 11:09:29 +0530

    About page is done image is left only

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,16 @@ const About = () => {
         </h1>
         {/* middle part is remening this is ony for middle part completion */}
 
-        <div className='w-full border-t-[1px] border-[#a1b562]'>
-
+        <div className='w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#a1b562]'>
+            <div className='w-1/2'>
+              <h1 className='text-7xl'>Our apporach:</h1>
+              <button className='flex uppercase items-center gap-10 px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white'>Read More
+                <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+              </button>
+            </div>
+            <div className="w-1/2 h-[70vh] rounded-3xl bg-[#90a549]">
+              <img src="" alt="one-man&-one-women-pic" />
+            </div>
         </div>
     </div>
   )
